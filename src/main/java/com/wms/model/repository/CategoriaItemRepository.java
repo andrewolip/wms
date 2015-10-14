b313ec09e6c175414505af8c58f98d28054f8e06
andrewolip <andrewolip@gmail.com>
2015-10-14 01:34:11 -0300

    SB Admin

---
+++ src/main/java/com/wms/model/repository/CategoriaItemRepository.java
@@ -0,0 +1,9 @@
+package com.wms.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.wms.model.entity.CategoriaItem;
+
+public interface CategoriaItemRepository extends CrudRepository<CategoriaItem, Integer> {
+
+}
---
+++ src/main/java/com/wms/model/repository/ObraRepository.java
@@ -0,0 +1,10 @@
+package com.wms.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.wms.model.entity.Obra;
+
+public interface ObraRepository extends CrudRepository<Obra, Integer>{
+	
+	
+}
