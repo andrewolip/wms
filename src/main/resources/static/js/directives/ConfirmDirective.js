c53be5b4725f696b7430fa9e08a03c50b431efda
Christian Raphael Pistori <chr.pistori@gmail.com>
2015-11-09 11:24:18 -0200

    Adicionada diretiva de confirmação de exclusão

---
+++ src/main/resources/static/js/directives/ConfirmDirective.js
@@ -0,0 +1,13 @@
+app.directive('ngConfirmClick', [ function() {
+		return {
+			link : function(scope, element, attr) {
+				var msg = attr.ngConfirmClick;
+				var clickAction = attr.confirmedClick;
+				element.bind('click', function(event) {
+					if (window.confirm(msg)) {
+						scope.$eval(clickAction)
+					}
+				});
+			}
+		};
+}]);
