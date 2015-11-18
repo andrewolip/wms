40f6c86cf2a14e25845fca8b284b4665299f5a26
Christian Raphael Pistori <chr.pistori@gmail.com>
2015-11-18 04:28:48 -0200

    Função: js adicionados à index.html e ao App.js

--- src/main/resources/static/js/App.js
+++ src/main/resources/static/js/App.js
@@ -26,4 +26,46 @@ app.config(function config($stateProvider) {
 		templateUrl: "pages/funcoes.html",
 		controller: "FuncaoController"
 	});
+	
+	$stateProvider.state("clientes", {
+		url: "/clientes",
+		templateUrl: "pages/clientes.html",
+		controller: "ClienteController"
+	});
+	
+	$stateProvider.state("colaboradores", {
+		url: "/colaboradores",
+		templateUrl: "pages/colaboradores.html",
+		controller: "ColaboradorController"
+	});
+	
+	$stateProvider.state("contas_bancarias", {
+		url: "/contas_bancarias",
+		templateUrl: "pages/contas_bancarias.html",
+		controller: "ContaBancariaController"
+	});
+	
+	$stateProvider.state("empresas", {
+		url: "/empresas",
+		templateUrl: "pages/empresas.html",
+		controller: "EmpresaController"
+	});
+	
+	$stateProvider.state("fornecedores", {
+		url: "/fornecedores",
+		templateUrl: "pages/fornecedores.html",
+		controller: "FornecedorController"
+	});
+	
+	$stateProvider.state("materiais", {
+		url: "/materiais",
+		templateUrl: "pages/materiais.html",
+		controller: "MaterialController"
+	});
+	
+	$stateProvider.state("usuarios", {
+		url: "/usuarios",
+		templateUrl: "pages/usuarios.html",
+		controller: "UsuarioController"
+	});
 });
