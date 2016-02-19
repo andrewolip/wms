a4374183aea054cf0d1541ab92bd41d665ac3a77
andrewolip <andrewolip@gmail.com>
2016-02-19 00:12:00 -0200

    UnidadeObraRepository editada

--- src/main/java/com/wms/model/repository/UnidadeObraRepository.java
+++ src/main/java/com/wms/model/repository/UnidadeObraRepository.java
@@ -1,13 +1,10 @@
 package com.wms.model.repository;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wms.model.entity.UnidadeObra;
 
 public interface UnidadeObraRepository extends JpaRepository<UnidadeObra, Integer>{
 
-	Collection<UnidadeObra> findByObraIdObra(Integer id);
 	
 }
